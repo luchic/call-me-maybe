f9ed2d0600437f2a0d8841919a4960b3b2685720
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-05-12 14:02:26 +0200

    Update install.sh file

--- setup/install.sh
+++ setup/install.sh
@@ -1,6 +1,9 @@
+export VENV="${HOME}/goinfre/venv"
 export CACHE="${HOME}/goinfre/.cache"
 export UV_CACHE_DIR="${CACHE}/uv"
 export HF_HOME="${CACHE}/hf"
 
-mkdir -p "$UV_CACHE_DIR" "$HF_HOME"
+export UV_PROJECT_ENVIRONMENT="${VENV}"
+
+mkdir -p "$UV_CACHE_DIR" "$HF_HOME" "$VENV"
 uv sync --python 3.11
